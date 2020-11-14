f67cc91f809f8b99b4216c4bb15a3349bde87f24
robincourant <courant.robin@hotmail.fr>
2020-11-14 16:34:35 +0100

    feat: data preparation
    - Add a method to load a dataset
    - Add a method to prepare data according to its type
    - Add a CLI to launch these methods
    - Create and update requirements
    - Create gitignore
    - Modify the README, by adding dataset descriptions

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Requirements :
 
-- python 3.8 
+- python 3.8
 - pip version ≥ 20.0
 
 ## Installation :
@@ -12,3 +12,51 @@
 pip install -r requirements.txt
 ```
 
+## Usage
+
+```
+python main.py dataset_filename
+```
+
+## Datasets
+
+### Boston Housing:
+
+Download this dataset [here](https://www.kaggle.com/altavish/boston-housing-dataset).
+
+There are 13 features to predict the price houses in Boston.
+
+Description:
+  - `CRIM`: per capita crime rate by town.
+  - `ZN`: proportion of residential land zoned for lots over 25,000 square feet.
+  - `INDUS`: proportion of non-retail business acres per town.
+  - `CHAS`: Charles River dummy variable (1 if tract bounds river; 0 otherwise).
+  - `NOX`: nitric oxides concentration (parts per 10 million).
+  - `RM`: average number of rooms per dwelling.
+  - `AGE`: proportion of owner-occupied units built prior to 1940.
+  - `DIS`: weighted distances to five Boston employment centres.
+  - `RAD`: index of accessibility to radial highways.
+  - `TAX`: full-value property-tax rate per $10,000.
+  - `PTRATIO`: pupil-teacher ratio by town.
+  - `B`: 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town.
+  - `LSTAT`: % lower status of population.
+  - `MEDV` (target): median value of owner-occupied homes in $1000's.
+
+
+### Prostate Cancer:
+
+Download this dataset [here](https://web.stanford.edu/~hastie/ElemStatLearn/datasets/prostate.data).
+The last column of the original dataset `train` has been removed.
+
+There are 8 features to predict a log of PSA (Prostate Specific Antigen) value, a marker for Prostate cancer.
+
+Description:
+  - `lcavol`: log of cancer volume.
+  - `lweight`: log of prostate weight.
+  - `age`: age of subject.
+  - `lbph`: log of benign prostatic hyperplasia amount.
+  - `svi`: seminal vesicle invasion.
+  - `lcp`: log of capsular penetration.
+  - `gleason`: Gleason score.
+  - `pgg45`: percentage Gleason scores 4 or 5.
+  - `lpsa` (target): log of PSA.
---
+++ data_preparation.py
@@ -0,0 +1,87 @@
+import os
+
+import pandas as pd
+from pandas.api.types import is_integer_dtype, is_float_dtype
+
+
+def load_dataset(filename: str) -> pd.DataFrame:
+    """Load a dataset from its filename.
+
+    :param filename: path to the dataset's `.csv` file.
+    :return: the loaded dataset in a dataframe.
+    """
+    dataset_df = pd.read_csv(filename)
+    return dataset_df
+
+
+def _prepare_float(
+    column_series: pd.Series, mean: bool = True, standardize: bool = True
+):
+    """
+    Fill missing values of a float column with its mean or median,
+    and standardize it.
+
+    :param column_series: column to process.
+    :param mean: whether to fill missing values with the mean or the median.
+    :param standardize: whether to standardize or normalize the column.
+    :return: the processed column.
+    """
+    # Fill missing values with the mean or the median of the column
+    filling_value = column_series.mean() if mean else column_series.median()
+    column_series.fillna(filling_value, inplace=True)
+
+    # Standardize the column
+    a = column_series.mean() if standardize else column_series.min()
+    b = column_series.std() if standardize else column_series.max() - a
+    column_series = column_series.apply(lambda x: (x - a) / b)
+
+    return column_series
+
+
+def _prepare_int(column_series: pd.Series, mean: bool = True):
+    """Fill missing values of an integer column with its mean or median.
+
+    :param column_series: column to process.
+    :param mean: whether to fill missing values with the mean or the median.
+    :return: the processed column.
+    """
+    # Fill missing values with the mean or the median of the column
+    filling_value = column_series.mean() if mean else column_series.median()
+    column_series.fillna(int(filling_value), inplace=True)
+
+    return column_series
+
+
+def _prepare_bool(column_series: pd.Series):
+    """Fill missing values of a bolean column with the most frequent value.
+
+    :param column_series: column to process.
+    :return: the processed column.
+    """
+    # Fill missing values with the mean or the median of the column
+    filling_value = column_series.mode()
+    column_series.fillna(int(filling_value), inplace=True)
+
+    return column_series
+
+
+def prepare_data(dataset_df: pd.DataFrame):
+    """Fill missing values and standardize float columns.
+
+    :param dataset_df: dataset to process.
+    """
+    for column_name, column_series in dataset_df.iteritems():
+        # Check whether the column type is boolean or integer
+        if is_integer_dtype(column_series):
+            if set(column_series.unique()) == {0, 1}:
+                dataset_df[column_name] = _prepare_bool(column_series)
+            else:
+                dataset_df[column_name] = _prepare_int(column_series)
+
+        # Check whether the column type is float
+        elif is_float_dtype(column_series):
+            dataset_df[column_name] = _prepare_float(column_series)
+
+        # Raise an error is the column type is not boolean, integer or float
+        else:
+            raise TypeError(f"Unrecognized type, column: {column_name}")
