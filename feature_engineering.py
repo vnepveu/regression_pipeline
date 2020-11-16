340befb7da1308cde2d2c483a4220f2cec920c6c
antoinelerosey <73841545+antoinelerosey@users.noreply.github.com>
2020-11-16 19:42:41 +0100

    review modification

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,55 +1,53 @@
-import numpy as np
-
+from sklearn.feature_selection import SelectFromModel, SelectKBest, f_regression
+from sklearn.linear_model import Ridge
 import pandas as pd
 
-from sklearn.feature_selection import SelectFromModel, SelectKBest,f_regression
-from sklearn.linear_model import Ridge
+from data_preparation import load_dataset, prepare_data
 
 
-
-from data_preparation import *
-
-def select_features_ridge(filename:str):
+def select_features_ridge(filename: str) -> pd.Index:
     """Select the relevant features using Ridge Regression method.
-    :param filename: path to the dataset's '.csv' file
-    :return: the selected features in an Index type
 
+    :param filename: path to the dataset's '.csv' file.
+    :return: the selected features.
     """
-    data_work = load_dataset(dataset)
+    data_work = load_dataset(filename)
     # Perform data preparation before selecting features
     prepare_data(data_work)
-    nb_columns = len(data_work.columns)
-    
+
     # Splitting label column from input column
-    label_column=data_work.iloc[:,-1] 
-    data_work=data_work.iloc[:,:-1]
+    label_column = data_work.iloc[:, -1]
+    data_work = data_work.iloc[:, :-1]
 
     # Using Ridge regression to select the relevant features
-    selection= SelectFromModel(Ridge())
-    selection.fit(data_work,label_column)
+    selection = SelectFromModel(Ridge())
+    selection.fit(data_work, label_column)
     selected_features = data_work.columns[(selection.get_support())]
+
     return selected_features
 
-def select_features_correlation(filename:str,number_of_feature_to_keep:int):
-    """Select the relevant features by looking at correlation
-    :param filename: path to the datset's '.csv' file
-    :param number_of_features_to_keep: number of top features to select
-    :return: the selected feature in an Index type
+
+def select_features_correlation(
+    filename: str, number_of_feature_to_keep: int
+) -> pd.Index:
+    """Select the relevant features by looking at correlation.
+
+    :param filename: path to the datset's '.csv' file.
+    :param number_of_features_to_keep: number of top features to select.
+    :return: the selected features.
     """
 
-    data_work = load_dataset(dataset)
+    data_work = load_dataset(filename)
     # Perform data preparation before selecting features
     prepare_data(data_work)
-    nb_columns = len(data_work.columns)
 
     # Splitting label column from input column
-    label_column = data_work.iloc[:,-1] 
-    data_work = data_work.iloc[:,:-1]
+    label_column = data_work.iloc[:, -1]
+    data_work = data_work.iloc[:, :-1]
 
-    # Using f_regression to select the relevant features
+    # Using `f_regression` to select the relevant features
     selection = SelectKBest(score_func=f_regression, k=number_of_feature_to_keep)
-    selection.fit(data_work,label_column)
+    selection.fit(data_work, label_column)
     selected_features = data_work.columns[(selection.get_support())]
-    return selected_features
-    
 
+    return selected_features
