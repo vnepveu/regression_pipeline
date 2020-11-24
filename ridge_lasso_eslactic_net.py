d7595098178a5f14dac204283f1f160d83361208
antoinelerosey <73841545+antoinelerosey@users.noreply.github.com>
2020-11-25 00:20:56 +0100

    deleting fit and predict

--- ridge_lasso_eslactic_net.py
+++ ridge_lasso_eslactic_net.py
@@ -1,57 +1,41 @@
 import numpy as np
 from sklearn.linear_model import Ridge, Lasso, ElasticNet
-from sklearn.metrics import mean_squared_error, r2_score
 
 
 def ridge_regression(
     x_train: np.array, y_train: np.array, x_test: np.array
-) -> np.array:
+) -> BaseEstimator:
     """Ridge regression prediction.
     :param x_train: Training set
     :param y_train:Training labels
     :param x_test: Test set
-    :return: prediction
+    :return: Ridge model
     """
     ridge = Ridge()
-    ridge.fit(x_train, y_train)
-    return ridge.predict(x_test)
+    return ridge
 
 
 def lasso_regression(
     x_train: np.array, y_train: np.array, x_test: np.array
-) -> np.array:
+) -> BaseEstimator:
     """Lasso regression prediction.
     :param x_train: Training set
     :param y_train:Training labels
     :param x_test: Test set
-    :return: prediction
+    :return: Lasso model
     """
     lasso = Lasso()
-    lasso.fit(x_train, y_train)
-    return lasso.predict(x_test)
+    return lasso
 
 
 def elastic_net_regression(
     x_train: np.array, y_train: np.array, x_test: np.array
-) -> np.array:
+) -> BaseEstimator:
     """ElsaticNet regression prediction.
     :param x_train: Training set
     :param y_train:Training labels
     :param x_test: Test set
-    :return: prediction
+    :return: ElasticNet model
     """
     elastic_net = ElasticNet()
-    elastic_net.fit(x_train, y_train)
-    return elastic_net.predict(x_test)
-
-
-def score(predictions: np.array, y_test: np.array) -> [float, float]:
-
-    """Mean squared error and r2 score of the model prediction.
-    :param predictions : Predictions from the model.
-    :param y_test : Test labels
-    :return: regression scores.
-    """
-    rmse = np.sqrt(mean_squared_error(y_test, predictions))
-    r2 = r2_score(y_test, predictions)
-    return rmse, r2
+    return elastic_net
